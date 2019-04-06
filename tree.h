fc2fec0813286dc05c5d9f051e0e3f169c5c4dfa
Simple <44000608+wWrongName@users.noreply.github.com>
2019-04-06 16:23:46 +0300

    Create tree.h

---
+++ tree.h
@@ -0,0 +1,22 @@
+#pragma once
+
+typedef struct NODE{
+	double  value;
+	int  weight;
+	struct NODE *left;
+	struct NODE *right;
+} NODE;
+
+void count_weight   (NODE *ptr);
+int  difference     (NODE *ptr);
+NODE *remove_least  (NODE *top);
+NODE *find_least    (NODE *ptr);
+NODE *left_to_right (NODE *top);
+NODE *right_to_left (NODE *top);
+NODE *twice_balanse (NODE *top);
+NODE *insert        (NODE *ptr, double val);
+NODE *n_remove      (NODE *ptr, double val);
+int search_for_twise(NODE *tree, double val);
+NODE *create_node   (double val);
+void error1();
+void error2();
